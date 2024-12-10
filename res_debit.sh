195c218fffa3dd06aa019d735d1a48cc03d10d9c
Faizan Quazi <39616180+sudofaizan@users.noreply.github.com>
2024-12-10 11:08:32 +0530

    Update res_debit.sh

--- res_debit.sh
+++ res_debit.sh
@@ -4,4 +4,3 @@ curl  -X POST http://localhost:80/message \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer faizanquazi"   \
 -d '{"message": "INR 10.00 debited\nA/c no. XX5067\n16-11-24, 11:38:40\nUPI/P2M/212326381087/LIFE MEDICO\nNot you? SMS BLOCKUPI Cust ID to 919951860002\nAxis Bank"}'
-
